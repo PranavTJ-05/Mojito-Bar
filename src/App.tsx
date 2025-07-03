77f775a8fb5b8aca46b128c770da48550aa6c22f
PranavTJ-05 <pranav.tj150605@gmail.com>
2025-07-03 16:07:47 +0530

    implemented the first part of the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './index.css';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from 'gsap';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
   )
 }
 
